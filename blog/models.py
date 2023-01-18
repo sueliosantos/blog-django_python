a8993fe05c29479bc36ddf30167a4f2672ccd716
Senhagit123 <sueliosantos@gmail..com>
2023-01-18 20:45:47 -0300

    Initial migrates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    data_publicacao= models.DateTimeField(default=datetime.now)
