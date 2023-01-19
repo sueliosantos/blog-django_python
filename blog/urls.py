c8aa4697c65a5894222dda06230df82debed893b
Senhagit123 <sueliosantos@gmail..com>
2023-01-18 22:27:50 -0300

    Pagina de posts e details

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('posts/<int:post_id>/post_detail', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 # Create your views
 
 
 def home(request):
-    return HttpResponse("<h1>Ola mundo</h1>")
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context)
