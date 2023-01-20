3ef920830199fe5ff84409fd73f36f0e917663d3
Senhagit123 <sueliosantos@gmail..com>
2023-01-19 21:33:00 -0300

    ckeditor

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 from django.utils import timezone
-
+from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
+    conteudo = RichTextUploadingField()
     data_publicacao= models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/<int:post_id>/post_detail', views.post_detail, name='post_detail')
